9fa29559d075f7d58a7021f6d7592e6e54e0a6cc
Philipp Gerbig <p.gerbig@sensory-minds.com>
2025-07-18 00:22:20 +0200

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'payload-maintenance-messages/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'payload-maintenance-messages/rsc'
+
+export const importMap = {
+  'payload-maintenance-messages/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'payload-maintenance-messages/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
